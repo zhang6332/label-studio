6763633ef6760a7ac4878eadf1f5536064d5997a
zhangjiahao <981163085@qq.com>
2026-06-26 12:48:59 +0800

    feat: rename Community edition to zjh edition

--- web/apps/labelstudio/src/pages/Home/HomePage.tsx
+++ web/apps/labelstudio/src/pages/Home/HomePage.tsx
@@ -228,7 +228,7 @@ export const HomePage: Page = () => {
           </SimpleCard>
           <div className="flex gap-2 items-center">
             <IconHumanSignal />
-            <span className="text-neutral-content-subtle">Label Studio Version: Community</span>
+            <span className="text-neutral-content-subtle">Label Studio Version: zjh</span>
           </div>
         </section>
       </div>
